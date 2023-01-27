1bd1fa70f19a8cedc740bd59b0efb13553acd4c9
Nguyễn Thế Thương <75439567+thuong9220022k2@users.noreply.github.com>
2023-01-27 21:18:02 +0700

    add branch

--- src/main/java/crawler/entity/FestivalCrawler.java
+++ src/main/java/crawler/entity/FestivalCrawler.java
@@ -1,8 +1,19 @@
 package crawler.entity;
 
 import crawler.BaseWebCrawler;
+import history.entity.Festival;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class FestivalCrawler extends BaseWebCrawler {
     public FestivalCrawler(List<String> urls) {
@@ -14,4 +25,50 @@ public class FestivalCrawler extends BaseWebCrawler {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	public static void main(String[] args) throws IOException  {
+		 Festival[] festival = new Festival[300];   
+		 ArrayList<String> listName = new ArrayList<String>();
+		 ArrayList<String> listTime = new ArrayList<String>();
+		 ArrayList<String> listAddress = new ArrayList<String>(); 
+		    
+		     String url = "https://www.couturetravelcompany.com/cac-le-hoi-o-viet-nam/";
+	    	   
+	    	   Document doc = Jsoup.connect(url).get() ;
+	    	   System.out.println(doc);
+	    	   
+	    	   for(Element row : doc.getElementsByTag("h4")) {
+	    		   listName.add(row.text());
+	    		   
+	    	   }
+	    	   listName.remove(listName.size()-1);
+	    	   
+	    	   
+	    	   for(Element row : doc.select("li:matches(Thời gian:)")) {
+	    		   listTime.add(row.text());	    		   
+	    		  
+	    	   }
+	    	   
+	    	   for(Element row : doc.select("li:matches(Địa điểm:)")) {
+	    		   listAddress.add(row.text());
+	   	   
+	    	   }
+	    	   for(int i=0;i<listName.size();i++) {
+	    		   festival[i] = new Festival();
+	    		   festival[i].setNameFes(listName.get(i));
+	    		   festival[i].setTimeFes(listTime.get(i));
+	    		   festival[i].setPlaceFes(listAddress.get(i));
+	    	   }
+	    	   Writer writer = new FileWriter("C:\\Users\\ASUS\\Documents\\ThuongOOP\\HistoryCrawler\\src\\main\\java\\crawler\\json\\festival.json", true);
+	    	   for(int i=0;i<listName.size();i++) {
+	    		   Gson gson = new GsonBuilder().create();
+			       gson.toJson(festival[i], writer);
+			       writer.write('\n');
+	    	   }
+	    	   System.out.println(listName.size());
+	    	   System.out.println(festival[listName.size()-1].getPlaceFes());
+	    	   
+	    	   
+	    	   
+	 }
 }
+
--- src/main/java/history/entity/Festival.java
+++ src/main/java/history/entity/Festival.java
@@ -3,5 +3,46 @@ package history.entity;
 import history.History;
 
 public class Festival extends History {
+	private String nameFes;
+	private String timeFes;
+	private String placeFes;
+		
+	public Festival() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public Festival(String nameFes, String timeFes, String placeFes) {
+		super();
+		this.nameFes = nameFes;
+		this.timeFes = timeFes;
+		this.placeFes = placeFes;
+	}
+
+	public String getNameFes() {
+		return nameFes;
+	}
+
+	public void setNameFes(String nameFes) {
+		this.nameFes = nameFes;
+	}
+
+	public String getTimeFes() {
+		return timeFes;
+	}
+
+	public void setTimeFes(String timeFes) {
+		this.timeFes = timeFes;
+	}
+
+	public String getPlaceFes() {
+		return placeFes;
+	}
+
+	public void setPlaceFes(String placeFes) {
+		this.placeFes = placeFes;
+	}
+	
+	
+	
+			
 }
