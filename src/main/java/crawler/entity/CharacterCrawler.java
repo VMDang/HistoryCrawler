7bd626b7977aaa237ac3185e23f7b856dc57887f
HungND1701 <Hung.nd204984@sis.hust.edu.vn>
2023-01-29 15:57:34 +0700

    29-01 16h

--- src/main/java/crawler/entity/CharacterCrawler.java
+++ src/main/java/crawler/entity/CharacterCrawler.java
@@ -6,7 +6,6 @@ import history.entity.Character;
 import java.io.IOException;
 import java.io.Writer;
 import java.io.FileWriter;
-import org.json.simple.JSONObject;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,6 +14,8 @@ import org.jsoup.select.Elements;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import java.util.*;
 
@@ -92,20 +93,28 @@ public class CharacterCrawler extends BaseWebCrawler {
 		return allUrl;
 	}
 	public void getdata_NKS(List<String> allUrl) {
+		try (Writer writer = new FileWriter("C:\\Users\\Acer\\Documents\\workspace\\JaVa\\History_Project2\\HistoryCrawler\\character.json", true)) {
+		    writer.write('[');
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 		for(String url : allUrl) {
+			Character nv = new Character();
 			try {
 				Document characterPage = Jsoup
 	                     				.connect(url)
 	                     				.userAgent("Jsoup client")
 	                     				.timeout(20000).get();
 				String name = characterPage.select("div.page-header h2").text();
+				
 				Elements infoBoxs = characterPage.select("div[class*=infobox]");
+				Elements articleBody = characterPage.select("div[itemprop=articleBody]"); 
+				String Sinh = null;
+				String Mat = null;
+				String description = "";
 				if(infoBoxs.size()> 0) {
 					Element infoTable = infoBoxs.get(0).select("table").get(0);
-					Elements rows = infoTable.select("tbody tr");
-					String Sinh = null;
-					String Mat = null;
-					String description = null;
+					Elements rows = infoTable.select("tbody tr");					
 					for(Element r : rows) {
 						Elements col1 = r.select("th");
 						Elements col2 = r.select("td");
@@ -126,18 +135,90 @@ public class CharacterCrawler extends BaseWebCrawler {
 							description += Value +"\n";
 						}
 					}
-					try (Writer writer = new FileWriter("C:\\Users\\Acer\\Documents\\workspace\\JaVa\\History_Project\\HistoryCrawler\\", true)) {
-					    Gson gson = new GsonBuilder().create();
-					    gson.toJson(event, writer);
-					    writer.write('\n');
+					nv.setName(name);
+					nv.setTime((Sinh==null?"Không rõ":Sinh.equals("?")?"Không rõ":Sinh) + "---" + (Mat==null?"Không rõ":Mat.equals("?")?"Không rõ":Mat));
+					Elements pTags = articleBody.select("div[class=infobox] ~ *");
+					if(pTags.size()>0) {
+						for(Element p : pTags) {
+							if(!p.text().equals("")) description+=p.text()+"\n";
+						}
+					}
+					nv.setDescription(description);
+					try (Writer writer = new FileWriter("C:\\Users\\Acer\\Documents\\workspace\\JaVa\\History_Project2\\HistoryCrawler\\character.json", true)) {
+					    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+					    gson.toJson(nv, writer);
+					    writer.write(",\n");
+					}catch(IOException e) {
+						e.printStackTrace();
 					}
 					
 				}else {
-					
+					Elements pTags = articleBody.select("p");
+					if(pTags.size()>0) {
+						String info = pTags.get(0).text();
+						String timeRegex ="("+name+" )"+"(\\()([^)]*)(\\))";
+						Pattern p = Pattern.compile(timeRegex);
+					    Matcher matcher = p.matcher(info);
+					    boolean matchFound = matcher.find();
+					    if(matchFound) {
+					      String nameTime = matcher.group();
+					      String Regex = "\\(.*\\)";
+					      Pattern pattern = Pattern.compile(Regex);
+					      Matcher m = pattern.matcher(nameTime);
+					      boolean match = m.find();
+					      String time = "Không rõ";
+					      if(match){
+					      	String str = m.group();
+					      	time = str.substring(1, str.length()-1);
+					      }
+					      nv.setName(name);
+					      nv.setTime(time);
+					      Elements pTags1 = articleBody.select("#toc ~ *");
+					      if(pTags1.size()>0) {
+							for(Element p1 : pTags1) {
+								if(!p1.text().equals("")) description+=p1.text()+"\n";
+							}
+					      }else {
+					    	  description = "";
+					      }
+					      nv.setDescription(description);
+					      try (Writer writer = new FileWriter("C:\\Users\\Acer\\Documents\\workspace\\JaVa\\History_Project2\\HistoryCrawler\\character.json", true)) {
+							    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+							    gson.toJson(nv, writer);
+							    writer.write(",\n");
+					      }catch(IOException e) {
+							e.printStackTrace();
+					      }
+					    }else {
+					    	nv.setName(name);
+					    	nv.setTime("Không rõ");
+					    	Elements pTags1 = articleBody.select("#toc ~ *");
+						      if(pTags1.size()>0) {
+								for(Element p1 : pTags1) {
+									if(!p1.text().equals("")) description+=p1.text()+"\n";
+								}
+						      }else {
+						    	  description = "";
+						      }
+						    nv.setDescription(description);
+						    try (Writer writer = new FileWriter("C:\\Users\\Acer\\Documents\\workspace\\JaVa\\History_Project2\\HistoryCrawler\\character.json", true)) {
+							    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+							    gson.toJson(nv, writer);
+							    writer.write(",\n");
+							}catch(IOException e) {
+								e.printStackTrace();
+							}
+					    }
+					}
 				}
 			} catch(IOException e) {
 				e.printStackTrace();
 			}
+		}
+		try (Writer writer = new FileWriter("C:\\Users\\Acer\\Documents\\workspace\\JaVa\\History_Project2\\HistoryCrawler\\character.json", true)) {
+		    writer.write(']');
+		}catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
 	public static void main(String[] args) {
