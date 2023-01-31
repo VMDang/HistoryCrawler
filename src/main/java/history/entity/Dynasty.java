b52754f0fe257b398902ae1c0b26b47da6255b3f
dungpt24082002 <dungpt24082002@gmail.com>
2023-02-01 00:01:45 +0700

    Update dynasty json file

--- src/main/java/history/entity/Dynasty.java
+++ src/main/java/history/entity/Dynasty.java
@@ -5,6 +5,21 @@ import history.History;
 public class Dynasty extends History {
 	
 	private String kingdom;
+	private String capital;
+	
+	public String getKingdom() {
+		return kingdom;
+	}
+	public void setKingdom(String kingdom) {
+		this.kingdom = kingdom;
+	}
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	
 	
 
 }
