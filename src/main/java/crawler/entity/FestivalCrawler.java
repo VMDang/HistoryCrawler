adb8719d36f5fe1a8ef6673c073429ba99075780
VMDang <dang.vm205063@sis.hust.edu.vn>
2023-02-03 15:52:59 +0700

    update to pull master

--- src/main/java/crawler/entity/FestivalCrawler.java
+++ src/main/java/crawler/entity/FestivalCrawler.java
@@ -138,8 +138,9 @@ public class FestivalCrawler extends BaseWebCrawler {
 		}
 
 	}
-	public static void main(String[] args) throws IOException {
-		FestivalCrawler.crawlerWiki("https://vi.wikipedia.org/wiki/L%E1%BB%85_h%E1%BB%99i_Vi%E1%BB%87t_Nam");
-		FestivalCrawler.crawlerBlog("https://www.couturetravelcompany.com/cac-le-hoi-o-viet-nam/");
-	}
+
+//	public static void main(String[] args) throws IOException {
+//		FestivalCrawler.crawlerWiki("https://vi.wikipedia.org/wiki/L%E1%BB%85_h%E1%BB%99i_Vi%E1%BB%87t_Nam");
+//		FestivalCrawler.crawlerBlog("https://www.couturetravelcompany.com/cac-le-hoi-o-viet-nam/");
+//	}
 }
--- src/main/java/crawler/manager/Main.java
+++ src/main/java/crawler/manager/Main.java
@@ -1,4 +1,12 @@
 package crawler.manager;
 
+import crawler.entity.FestivalCrawler;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        FestivalCrawler.crawlerWiki("https://vi.wikipedia.org/wiki/L%E1%BB%85_h%E1%BB%99i_Vi%E1%BB%87t_Nam");
+        FestivalCrawler.crawlerBlog("https://www.couturetravelcompany.com/cac-le-hoi-o-viet-nam/");
+    }
 }
